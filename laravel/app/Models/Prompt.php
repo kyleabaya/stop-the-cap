997b1eecf5b464a1067a8424136907d92365f345
mayalaidler <mayalaidler@gmail.com>
2025-03-31 13:38:32 +0900

    promptname change

---
+++ laravel/app/Models/Prompt.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class prompts extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'prompt_text',
+        'prompt_type'
+    ];
+
+    public function rounds()
+    {
+        return $this->hasMany(Round::class);
+    }
+}
